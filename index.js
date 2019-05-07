d8780253287dd099591754e99b24942e603cd844
benoitDD <benoitdegorsas@outlook.fr>
2019-05-07 14:51:50 +0200

    b15

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const app = require('express')()
 const PORT = 3000
 
 app.get('/', (req, res) => {
-    res.send('Hello :~)')
+    res.send('Hello :~) and CI:', process.env.CI)
 })
 
 app.listen(PORT, () => {
-    console.log('App ready on port ', PORT)
+    console.log(`App on mode ${process.env.CI} ready on port ${PORT}`)
 })
