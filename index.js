5a9c81520eecf363d52a097e33b98411e3f51075
benoitDD <benoitdegorsas@outlook.fr>
2019-05-08 12:51:20 +0200

    b21

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
     res.send(`Hello :~) and CI: ${process.env.CI}, with secret: ${process.env.UN_SECRET}`)
 })
 
+app.get('/secrets', (req, res) => {
+    res.send(`PORT :${process.env.PORT}, DB_LOGIN :${process.env.DB_LOGIN}, DB_PASSWORD :${process.env.DB_PASSWORD}`)
+})
+
 app.listen(PORT, () => {
     console.log(`App on mode CI: ${process.env.CI}, with secret: ${process.env.UN_SECRET} ready on port ${PORT}`)
 })
